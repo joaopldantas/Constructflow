11ecc18994db3d2c84a25a7ee1618cc880cadaa3
joaopldantas <joaopedrold8@gmail.com>
2026-02-11 08:44:38 -0300

    Add UsuarioController with CRUD endpoints

--- src/main/java/joaopldantas/project/controllers/UsuarioController.java
+++ src/main/java/joaopldantas/project/controllers/UsuarioController.java
@@ -1,8 +1,11 @@
 package joaopldantas.project.controllers;
 
+import joaopldantas.project.entities.Usuario;
 import joaopldantas.project.services.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -13,10 +16,59 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
-    // POST /usuarios (criar)
-    // GET /usuarios/{id} (buscar)
-    // GET /usuarios/email?email=joao@email.com (buscar)
-    // GET /usuarios (listar todos)
-    // PUT /usuarios/{id} (atualizar)
-    // DELETE /usuarios/{id} (deletar)
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrarUsuario(
+            @RequestBody Usuario usuario) {
+
+        Usuario criado = usuarioService.criarUsuario(usuario);
+        return ResponseEntity.status(201).body(criado);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(
+            @PathVariable Long id) {
+        return usuarioService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<Usuario> buscarPorEmail(
+            @RequestParam String email){
+        return usuarioService.buscarPorEmail(email)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarTodos() {
+        return ResponseEntity.ok(usuarioService.listarTodos());
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> atualizarUsuario(
+            @PathVariable Long id,
+            @RequestBody Usuario usuarioAtualizado) {
+
+        return usuarioService.buscarPorId(id)
+                .map(usuarioExistente -> {
+                    Usuario atualizado = usuarioService.atualizarUsuario(id, usuarioAtualizado);
+                    return ResponseEntity.ok(atualizado);
+                })
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(
+            @PathVariable Long id) {
+
+        if (usuarioService.buscarPorId(id).isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
 }
